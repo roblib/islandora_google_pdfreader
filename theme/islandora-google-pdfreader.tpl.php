017c6fb720498d841f28aa686f0016901bc6d027
Paul Pound <ppound@upei.ca>
2013-08-23 13:37:22 -0300

    initial commit

---
+++ theme/islandora-google-pdfreader.tpl.php
@@ -0,0 +1,6 @@
+<div class="islandora-content-wrapper">
+  <div class="islandora-google-pdfreader-iframe-wrapper">
+    <iframe src="<?php print $variables['viewer_url']; ?>" style="width:600px; height:500px;" frameborder="0"></iframe>
+    </div>
+</div>
+
