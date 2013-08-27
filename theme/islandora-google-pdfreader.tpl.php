c042608d9ec47b778dc1e27c4e324117455bd90f
Paul Pound <ppound@upei.ca>
2013-08-27 09:56:31 -0300

    code clean up

--- theme/islandora-google-pdfreader.tpl.php
+++ theme/islandora-google-pdfreader.tpl.php
@@ -1,6 +1,13 @@
+<?php
+/**
+ * @file
+ * load the google reader via an iframe.
+ */
+
+?>
 <div class="islandora-content-wrapper">
   <div class="islandora-google-pdfreader-iframe-wrapper">
-    <iframe src="<?php print $variables['viewer_url']; ?>" style="width:600px; height:500px;" frameborder="0"></iframe>
+    <iframe src="<?php print $variables['viewer_url']; ?>" style="width:600px; height:600px;" frameborder="0"></iframe>
     </div>
 </div>
 
