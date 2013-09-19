c5f4093cd2d44cb9db1247f071d55aab24bfc7a0
ekennedy <evkennedy@upei.ca>
2013-09-19 12:32:53 -0300

    Read with Download tab in progress.
    Signed-off-by: ekennedy <evkennedy@upei.ca>

--- theme/islandora-google-pdfreader.tpl.php
+++ theme/islandora-google-pdfreader.tpl.php
@@ -3,11 +3,13 @@
  * @file
  * load the google reader via an iframe.
  */
-
 ?>
+
+<?php if ($variables['show_viewer']): ?>
 <div class="islandora-content-wrapper">
   <div class="islandora-google-pdfreader-iframe-wrapper">
     <iframe src="<?php print $variables['viewer_url']; ?>" style="width:600px; height:600px;" frameborder="0"></iframe>
     </div>
 </div>
+<?php endif; ?>
 
