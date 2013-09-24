edc409d931901172d2aaed2a23bd8a9bc975eef6
ekennedy <evkennedy@upei.ca>
2013-09-24 13:36:01 -0300

    Read with Download tab V1.1
    - added css/islandora_google_pdfreader.css
    - all inline styles have been moved to islandora_google_pdfreader.css
    - table formatting removed
    - module now aware of DSID when considering access
    
    Signed-off-by: ekennedy <evkennedy@upei.ca>

--- theme/islandora-google-pdfreader.tpl.php
+++ theme/islandora-google-pdfreader.tpl.php
@@ -6,28 +6,22 @@
 ?>
 
 <?php if ($variables['show_viewer']): ?>
-<div class="islandora-content-wrapper">
-  <div class="islandora-google-pdfreader-iframe-wrapper">
-    <table>
-      <tr>
-        <td style=width:50%>
-          <iframe src="<?php print $variables['viewer_url']; ?>" style="width:600px; height:600px;" frameborder="0"></iframe>
-        </td>
+  <div class="islandora-content-wrapper">
+    <div class="islandora-google-pdfreader-iframe-wrapper">
+      <iframe src="<?php print $variables['viewer_url']; ?>"></iframe>
         <?php if (isset($variables['pdf_download_path'])): ?>
-          <td style="width:50%; vertical-align:top;">
         <?php endif; ?>
     </div>
 </div>
-<br/>
 <?php endif; ?>
 
 <?php if (isset($variables['pdf_download_path'])): ?>
-  <div style="float:left; position:relative; left:20px;">
+  <br/>
+  <div class="islandora-download-image-wrapper">
     <a href="<?php print $variables['pdf_download_path'] ?>">
       <img src="<?php print $variables['pdf_download_image'] ?>"/></a>
   </div>
-  <div style="position:relative; left:35px">
+  <div class="islandora-download-text-wrapper">
     <a href="<?php print $variables['pdf_download_path'] ?>"><br/>Download</a>
   </div>
 <?php endif; ?>
-</td></tr></table>
